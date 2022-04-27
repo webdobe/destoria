a06cedab4efe0210568b3214ec1fda375c353044
eroncardoso88 <deandradeeron@gmail.com>
2022-04-27 15:54:57 -0300

    header desktop

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import s from "./styles";
+import Image from 'next/image'
 
 const Header: FunctionComponent = function () {
   return (
@@ -8,7 +9,7 @@ const Header: FunctionComponent = function () {
       <h2 {...s.pageDescription}>Explore the Metaverse</h2>
       <div {...s.container}>
         <div {...s.logo}>
-          LOGO
+          <Image src="/destoria.svg" alt="Burger Menu" width={47} height={42} />
         </div>
         <div {...s.navBox}>
           <nav {...s.nav}>
@@ -21,8 +22,12 @@ const Header: FunctionComponent = function () {
             </ul>
           </nav>
           <div {...s.connectWallet}>
-            Connect Wallet
+            <span {...s.connectWalletText} style={{whiteSpace: 'nowrap', position: 'relative', left: '20px'}}>Connect Wallet</span>
+            <Image {...s.connectWalletImage} src="/connect-wallet.svg" alt="Burger Menu" width={200} height={45} style={{position: 'absolute'}}/>
           </div>
+        </div>
+        <div {...s.navBurger}>
+          <Image src="/burger.svg" alt="Burger Menu" width={72} height={24} />
         </div>
       </div>
     </header>
--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -1,14 +1,18 @@
 import classNameAdder from 'utils/classNameAdder'
 
 const styles = {
-    header: "mx-auto text-dorange",
+    header: "mx-auto text-dhOrange px-7",
     pageName: "hidden",
     pageDescription: "hidden",
     container: "w-full flex h-24 justify-between items-center text-base",
-    navBox: "flex items-center grow max-w-6xl justify-end",
+    navBox: "hidden md:flex items-center grow max-w-6xl justify-end gap-8 pb-2 text-xl font-bold tracking-wide",
+    logo: "p-1",
     nav: "grow flex justify-end",
-    navUl: "flex space-x-4 grow justify-end",
-    connectWallet: "pl-4"
+    navUl: "flex grow justify-end gap-9",
+    connectWallet: "w-45 h-10 flex justify-center items-center cursor-pointer ",
+    navBurger: "md:hidden",
+    connectWalletText: "w-0 glower",
+    connectWalletImage: "w-0"
 }
 
 
