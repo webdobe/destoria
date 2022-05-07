bde9a29d61fa73007c51ee18730c84cf08450aeb
eroncardoso88 <deandradeeron@gmail.com>
2022-05-07 15:40:09 -0300

    fix backgrounds

--- components/Home/CharactersCarousel/index.tsx
+++ components/Home/CharactersCarousel/index.tsx
@@ -33,21 +33,58 @@ const CharactersCarousel: FunctionComponent = function () {
             >
               <SwiperSlide>
                 <div className="flex flex-col items-center  w-full">
-                  <div className="flex flex-col items-center w-full lg:px-40">
-                    <div {...s.charactersContent}>
+                  <div {...s.charactersContent}>
+                    <div {...s.charactersImageContent} style={{minWidth: '360px'}}>
                       <Image
                         {...s.characterImage}
                         src="/characters-outrider-1.png"
                         alt="Burger Menu"
-                        width={200}
-                        height={45}
+                        width={360}
+                        height={360}
+                        style={{ position: "absolute", minWidth: '360px' }}
+                      />
+                    </div>
+                    <div {...s.charactersTextContent}>
+                      <h5 {...s.charactersTextContentTitle}>Faction</h5>
+                      <h4 {...s.charactersTextContentSubtitle}>Outrider</h4>
+                      <p  {...s.charactersTextContentText}>
+                        The Destorian Outriders are an organization of rangers
+                        that keep the peace in all the southern and central
+                        territories of Ordos Prime. They are characterized by
+                        their pneumatic weaponry, tactical armor and gas masks.
+                        Alongside the long coats and hats they wear as part of
+                        their uniform. They are often seen patrolling Ordos
+                        Prime on their newly developed hover bikes.
+                      </p>
+                      <Image
+                        {...s.characterImage}
+                        src="/characters-outrider-status.svg"
+                        alt="Burger Menu"
+                        width={360}
+                        height={71}
                         style={{ position: "absolute" }}
                       />
                     </div>
-                    <div>
-                      <h5>Faction</h5>
-                      <h4>Outrider</h4>
-                      <p>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="flex flex-col items-center  w-full">
+                  <div {...s.charactersContent}>
+                    <div {...s.charactersImageContent} style={{minWidth: '360px'}}>
+                      <Image
+                        {...s.characterImage}
+                        src="/characters-outrider-1.png"
+                        alt="Burger Menu"
+                        width={360}
+                        height={360}
+                        style={{ position: "absolute", minWidth: '360px' }}
+                      />
+                    </div>
+                    <div {...s.charactersTextContent}>
+                      <h5 {...s.charactersTextContentTitle}>Faction</h5>
+                      <h4 {...s.charactersTextContentSubtitle}>Outrider</h4>
+                      <p  {...s.charactersTextContentText}>
                         The Destorian Outriders are an organization of rangers
                         that keep the peace in all the southern and central
                         territories of Ordos Prime. They are characterized by
@@ -65,26 +102,6 @@ const CharactersCarousel: FunctionComponent = function () {
                         style={{ position: "absolute" }}
                       />
                     </div>
-                  </div>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="cards-carousel cards-carousel-left flex flex-col pt-10 md:pt-20 items-center">
-                  <div className="flex flex-col items-center w-full pl-14 pr-20 md:pl-20 md:pr-48">
-                    <h4 {...s.cardTitle}>Metaverse</h4>
-                    <p {...s.cardContent} style={{ marginBottom: "20px" }}>
-                      Users can set up an island with a passive income shop, or
-                      host a P2E experience. Be the Destorian Mechanic or a
-                      Landlord and rent out housing. Use this space as not only
-                      a safe spot within our ecosystem, but also a place for you
-                      to grow with us.
-                    </p>
-                    <h4
-                      {...s.cardBottom}
-                      style={{ color: "#F0F0F0", filter: "blur(1px)" }}
-                    >
-                      coming soon...
-                    </h4>
                   </div>
                 </div>
               </SwiperSlide>
--- components/Home/CharactersCarousel/styles.ts
+++ components/Home/CharactersCarousel/styles.ts
@@ -1,11 +1,16 @@
 import classNameAdder from 'utils/classNameAdder'
 
 const styles = {
-    cardsCarousel: "w-full flex flex-col justify-center items-center text-dwhite bg-no-repeat bg-cover",
+    charactersCarousel: "w-full flex flex-col justify-center text-dwhite items-center text-dwhite bg-no-repeat bg-cover bg-characters",
     contentLayer: "w-full h-full relative m-auto",
     containerCharactersCarousel: "cards-carousel-container m-auto",
     containerCharactersCarouselBordered: "layout-border md:py-32 md: md:mx-5 m-auto",
-    charactersContent: "flex "
+    charactersContent: "flex flex-row text-dwhite lg:px-72 gap-5",
+    charactersImageContent: "flex flex-row text-dwhite w-72 ",
+    charactersTextContent: "",
+    charactersTextContentTitle: "text-dhOrange text-base uppercase tracking-widest",
+    charactersTextContentSubtitle: "font-bold capitalize tracking-wide text-4xl py-4",
+    charactersTextContentText: "text-base tracking-wide pb-5"
 }
 
 
