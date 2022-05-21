693e5672d1ed80bee038bb552f03f47c3958ec12
eroncardoso88 <deandradeeron@gmail.com>
2022-05-21 11:09:36 -0300

    added jobs

--- jobs/design.ts
+++ jobs/design.ts
@@ -1,26 +1,32 @@
+import { m } from "framer-motion"
+
 const Design = [
   {
-    name: "Design specialist",
+    name: "3d Modeler",
     field: "Design",
     id: 355,
     location: "Remote",
+    real: true,
     contractType: "Freelance",
-    experience: "5 years min",
-    jobDescription: "Job description lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
-    yourResponsibilities: "Job description lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
+    experience: "3 years min",
+    jobDescription: "Do you want to be the one credited with creating a body of work on a brand-new game project focused on evolving the current Metaverse and P2E global market? Working closely with other team members to develop game prototypes that define the promise of our company's ethos. Building organizational confidence in the project and facilitating efficient progress through the R&D development cycle. Well, here is that exciting opportunity for you to join an early project as a 3D character modeler, bringing characters & environments to life while helping to forge the game’s core look and art style with visual-development explorations and execution. This is a true team project that will take your suggestions and artistic style into consideration. You will never feel like it is just another job as part of the Destorian team.",
+    yourResponsibilities: [
+     " Create a variety of different  AAA style characters with clothing and fine details (Hair cards included)",
+"A high attention to detail and strong problem-solving abilities are equally important for the position",
+"Set the materials inside unreal with SSD and custom skin shaders",
+"Ability to follow reference material to create clean, detailed and accurate models"
+    ],
     skills: [
-      "Job description lorem ipsum dolor sit amet",
-      "Mattis tellus. Sed dignissim, metus nec fringilla accumsan",
-      "Risus sem sollicitudin lacus, ut interdum tellus elit sed risus",
-      "Maecenas eget condimentum velit, sit amet feugiat lectus",
-      "Class aptent taciti sociosqu ad litora tor"
+      "Proficiency in Unreal Engine",
+      "+ 3 Years working in the gaming industry",
+      "High knowledge on character creation and anatomy",
+      "Understanding of proportion, depth, & scale",
+      "Able to do Voice calls in English",
+      "Must have updated portfolio",
+      "Knowledge of rigging in Unreal Engine is a plus"
     ],
     plus: [
-      "Job description lorem ipsum dolor sit amet",
-      "Mattis tellus. Sed dignissim, metus nec fringilla accumsan",
-      "Risus sem sollicitudin lacus, ut interdum tellus elit sed risus",
-      "Maecenas eget condimentum velit, sit amet feugiat lectus",
-      "Class aptent taciti sociosqu ad litora tor"
+    
     ],
     locationDescription: "Anywhere in the world, our job opportunities are 100% remote!"
   },
@@ -29,6 +35,7 @@ const Design = [
     field: "Design",
     id: 356,
     location: "Remote",
+    real: false,
     contractType: "Freelance",
     experience: "5 years min",
     jobDescription: "Job description lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
@@ -54,9 +61,12 @@ const Design = [
     field: "Design",
     id: 357,
     location: "Remote",
+    real: false,
     contractType: "Freelance",
     experience: "5 years min",
-    jobDescription: "Job description lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
+    jobDescription: "Are you an experienced UI Programmer ready to join our newly ever growing and evolving indie game company Destoria? You will join a small but fast-growing team. You will enjoy successfully planning and implementing new UI systems for games and have experience implementing UI to the highest standard with the ability to write robust, clean and maintainable C++ code. You will work closely with our artists and design leads to ensure that UI meets the aesthetic and functional requirements for Destoria. As a full team member you will be given the opportunity to contribute ideas toward all aspects of the game’s inception and development.
+    On our team, your player-focused mindset allows you to conceptualize intuitive menus whose look and feel encompass our game's overall creative vision. Being a hands-on immersed designer you ensure that the gamer experience remains intuitive and forever immersive and provides our players with the correct and timely information they require.
+    ",
     yourResponsibilities: "Job description lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
     skills: [
       "Job description lorem ipsum dolor sit amet",
@@ -79,6 +89,7 @@ const Design = [
     field: "Design",
     id: 358,
     location: "Remote",
+    real: false,
     contractType: "Freelance",
     experience: "5 years min",
     jobDescription: "Job description lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar.",
