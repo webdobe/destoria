e9302e4c677103139505c97d95fd7f22a99f2413
eroncardoso88 <deandradeeron@gmail.com>
2022-05-28 17:13:44 -0300

    fix targetId production

--- components/Header/dropdownRoot.tsx
+++ components/Header/dropdownRoot.tsx
@@ -19,7 +19,7 @@ function DropdownRoot() {
     const { optionCenterX, contentDimensions } = cachedOption;
     width = contentDimensions?.width;
     height = contentDimensions?.height;
-    x = optionCenterX - width / 2 - (targetId == 2 ? 35 : targetId == 4 ? 30 : targetId == 6 ? 35 : targetId == 8 ? 30 : targetId == 10 ? 30 : 35);
+    x = optionCenterX - width / 2 - (targetId == 2 ? 35 : targetId == 4 ? 30 : targetId == 6 ? 35 : targetId == 8 ? 30 : targetId == 35 ? 30 : 35);
   }
 
   const [hovering, setHovering] = useState(false);
