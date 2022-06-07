963ca2777101afa46ebb48816dd3af9754a6521a
Jesse Longacre <jlongac2@its.jnj.com>
2022-06-06 18:29:03 -0700

    Remove margin so border shows.

--- components/Home/CardsCarousel/styles.ts
+++ components/Home/CardsCarousel/styles.ts
@@ -1,7 +1,7 @@
 import classNameAdder from 'utils/classNameAdder'
 
 const styles = {
-    cardsCarousel: "flex flex-col justify-center items-center text-dwhite bg-no-repeat bg-cover bg-full-forest relative mb-20",
+    cardsCarousel: "flex flex-col justify-center items-center text-dwhite bg-no-repeat bg-cover bg-full-forest relative",
     photoLayer: "w-full h-full absolute first-img-layer",
     contentLayer: "w-full h-full relative ",
     containerCardsCarousel: "cards-carousel-container m-auto h-auto",
